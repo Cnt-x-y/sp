b32e1f4515a98843516ff5e8d928a5520abeaa1e
2260387897@qq.com <2260387897@qq.com>
2019-09-02 12:12:10 +0800

    1

---
+++ common/src/main/java/com/xtt/common/CommonConstant.java
@@ -0,0 +1,10 @@
+package com.xtt.common;
+
+/**
+ * @Author xuett
+ * @Date 2019/9/2 12:10
+ */
+public final class CommonConstant {
+
+    public static final String PRODUCT_CENTER = "product-center";
+}
--- manager/src/main/java/com/xtt/manager/service/HelloServiceFeignClient.java
+++ manager/src/main/java/com/xtt/manager/service/HelloServiceFeignClient.java
@@ -1,5 +1,6 @@
 package com.xtt.manager.service;
 
+import com.xtt.common.CommonConstant;
 import com.xtt.productcenter.web.HelloServiceRemoteApi;
 import org.springframework.cloud.openfeign.FeignClient;
 
@@ -7,6 +8,6 @@ import org.springframework.cloud.openfeign.FeignClient;
  * @Author xuett
  * @Date 2019/9/2 11:26
  */
-@FeignClient(value = "product-center",fallback = HelloServiceFeignClientFallback.class)
+@FeignClient(value = CommonConstant.PRODUCT_CENTER, fallback = HelloServiceFeignClientFallback.class)
 public interface HelloServiceFeignClient extends HelloServiceRemoteApi {
 }
